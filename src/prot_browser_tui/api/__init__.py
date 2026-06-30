690f47c21662074f451207598e758e166ae56189
Andy <andymucyo@gmail.com>
2026-06-30 16:07:19 +0200

    FEAT added extract_display_data function

--- src/prot_browser_tui/api/__init__.py
+++ src/prot_browser_tui/api/__init__.py
@@ -1,5 +1,6 @@
-from .proteins import get_search_results
+from .proteins import get_search_results, extract_display_data 
 
 __all__ = [
         "get_search_results",
+        "extract_display_data"
         ]
--- src/prot_browser_tui/api/proteins.py
+++ src/prot_browser_tui/api/proteins.py
@@ -8,7 +8,8 @@ uniprot_api_url: str = "https://rest.uniprot.org/uniprotkb/search"
 async def get_search_results(url: str, prot_name: str, size: int=5) -> dict[str, Any] :
     payload = {
             "query": prot_name,
-            "size": 1,
+            "facets": "reviewed",
+            "size": size,
             "fields": ["accession", "id", "protein_name", "organism_name"],
             }
     async with httpx.AsyncClient() as client:
@@ -19,12 +20,38 @@ async def get_search_results(url: str, prot_name: str, size: int=5) -> dict[str,
                     "Content-Type": "application/json"
                     }
                 )
-        
-        print("status code: ", response.status_code)
-        return response.json()["results"][0]
+        response.raise_for_status()
+        return response.json()["results"]
 
+
+def extract_display_data(response_json: dict[str: Any]):
+    display_data: dict[str, dict[str, str]] = {}
+    entry_count: int = 1
+
+    for entry in response_json:
+        display_data[f"entry n°{entry_count}"] = {
+                "Accession": entry["primaryAccession"], 
+                "UniprotKB ID": entry["uniProtkbId"],
+                "Organism name": f"{entry["organism"]["scientificName"]} ({entry["organism"]["commonName"]})",
+                "Common Name": entry["proteinDescription"]["recommendedName"]["fullName"]["value"],
+                                                }
+        entry_count += 1
+
+    return display_data
 
 
 if __name__ == "__main__":
-    # print("Status code: ",get_status_code(uniprot_api_url, "insulin"))
-    print(asyncio.run(get_search_results(uniprot_api_url, "insulin")))
+    response = asyncio.run(get_search_results(uniprot_api_url, "insulin"))
+    display_data = extract_display_data(response)
+
+    for key in display_data.keys():
+        print(f"{key}: {display_data[key]}")
+    
+
+    # for i in range(0, len(response)):
+    #     entry = response[i]
+    #     print("--------------")
+    #     print(f"entry n°{i + 1}")
+    #     print("--------------")
+    #     for key in entry.keys():
+    #         print(f"{key}: {entry[key]}\n")
